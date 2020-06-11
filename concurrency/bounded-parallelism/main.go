c06781cd298553c5e20a71d08d8fe6854565de47
勇 张 <1451670604@qq.com>
2020-06-11 21:45:31 +0800

    docs: add main function

--- concurrency/bounded-parallelism/main.go
+++ concurrency/bounded-parallelism/main.go
@@ -8,6 +8,10 @@ import (
 	"io/ioutil"
 )
 
+func main() {
+
+}
+
 type result struct {
 	path string
 	sum [md5.Size]byte
@@ -46,7 +50,7 @@ func digester(done <-chan struct{}, paths <-chan string, c chan<- result) {
 	for path := range paths {
 		data, err := ioutil.ReadFile(path)
 		select {
-		case c <- result{path, md5.Sum(data), err}
+		case c <- result{path, md5.Sum(data), err}:
 		case <- done:
 			return
 		}
