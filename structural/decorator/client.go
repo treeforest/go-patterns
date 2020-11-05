bf178f52c46a4cd1fd31510eabeb47ffe5fefd7b
treeforest <1451670604@qq.com>
2020-11-05 20:52:11 +0800

    fix: decorator

--- structural/decorator/client.go
+++
@@ -1,9 +0,0 @@
-package main
-
-type Window struct {
-
-}
-
-func main() {
-
-}
--- structural/decorator/decorator.go
+++ structural/decorator/decorator.go
@@ -2,75 +2,32 @@ package main
 
 import "fmt"
 
-// 可视化组件接口
-type VisualComponent interface {
-	Draw()
-	Resize()
-}
+type Object func(int) int
 
-type Decorator struct {
-	Component VisualComponent
-}
+func CalculateDecorate(o Object) Object {
+	return func(n int) int {
+		fmt.Println("Starting the execution with the integer : ", n)
 
-func NewDecorator(comp VisualComponent) VisualComponent {
-	return &Decorator{
-		Component: comp,
+		result := o(n)
+
+		fmt.Println("Execution is completed  with the result : ", result)
+
+		return result
 	}
 }
 
-func (dec *Decorator) Draw() {
-	dec.Component.Draw()
+func Double(n int) int {
+	return n * 2
 }
 
-func (dec *Decorator) Resize() {
-	dec.Component.Resize()
+func Square(n int) int {
+	return n * n
 }
 
-// 边框装饰器
-type BorderDecorator struct {
-	Decorator
-	width int
-}
+func main() {
+	o := CalculateDecorate(Double)
+	o(10)
 
-func NewBorderDecorator(comp VisualComponent, borderWidth int) VisualComponent{
-	bd := new(BorderDecorator)
-	bd.Component = comp
-	bd.width = borderWidth
-	return bd
-}
-
-func (dec *BorderDecorator) Draw() {
-	dec.Decorator.Draw()
-	dec.drawBorder()
-}
-
-func (dec *BorderDecorator) drawBorder() {
-	fmt.Printf("绘制大小为 %d*%d 的边框\n", dec.width, dec.width)
-}
-
-// 滚动条装饰器
-type ScrollDecorator struct {
-	Decorator
-}
-
-func (dec *ScrollDecorator) Draw() {
-	dec.Decorator.Draw()
-	dec.drawScroll()
-}
-
-func (dec *ScrollDecorator) drawScroll() {
-	fmt.Println("绘制滚动条")
-}
-
-// 阴影装饰器
-type DropShadowDecorator struct {
-	Decorator
-}
-
-func (dec *DropShadowDecorator) Draw() {
-	dec.Decorator.Draw()
-}
-
-func (dec *DropShadowDecorator) drawDropShadow() {
-	fmt.Println("绘制阴影效果")
+	o = CalculateDecorate(Square)
+	o(10)
 }
